454d6a5aebc7f79e992d603ae71ae6bfb550a3d3
tofaeltopu <tofaelahmedtopu6@gmail.com>
2024-12-13 15:27:39 +0600

    first comit

---
+++ biodata.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+    //printing my bio data
+    cout<<"\"tofael ahmed topu\"\n" ;
+    cout<<"kishoregonj\n";
+    cout<<"01640485551\n";
+    cout<<" student";
+
+    getch();
+
+
+}
---
+++ my name.c
@@ -0,0 +1,8 @@
+#include<iostream;
+#include<conic.h;
+using namespace std;
+int main()
+{
+    cout
+}
+
